bfa1f72531b854ed513a827153bbfad4b22af90a
Justine B <justinegayton@gmail.com>
2019-03-04 18:27:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Code your solution in this file.
 
-const fullnames = [
-        'Bobby Smith',
-        'Sammy Watkins',
-        'Sally Jenkins',
-        'Annette Sawyer',
-        'Sarah Hucklebee',
-        'bobby ridge'
-      ];
+// const fullnames = [
+//         'Bobby Smith',
+//         'Sammy Watkins',
+//         'Sally Jenkins',
+//         'Annette Sawyer',
+//         'Sarah Hucklebee',
+//         'bobby ridge'
+//       ];
 
 function lowerCaseDrivers(drivers) {
   return drivers.map (driver => driver.toLowerCase());
