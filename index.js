5ce959cdcd23cc0195db98b5bf914013450f749d
Justine B <justinegayton@gmail.com>
2019-03-04 18:49:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function lowerCaseDrivers(drivers) {
-  return drivers.map (driver => driver.toLowerCase());
+  return drivers.map(driver => driver.toLowerCase());
 }
 
 function nameToAttributes(drivers) {
-    return drivers.map ( function (driver) {
+  return drivers.map(function (driver) {
       const fullname = driver.split(" ");
       let firstname = fullname[0]
       let lastname = fullname[1]
