679acaec3349f6f10e908c774497cd5ff8583540
Justine B <justinegayton@gmail.com>
2019-03-04 18:52:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ function lowerCaseDrivers(drivers) {
   return drivers.map(driver => driver.toLowerCase());
 }
 
-function nameToAttributes(drivers) {
-  return drivers.map(function(driver) {
-    const fullname = driver.split(" ");
-    const firstname = fullname[0];
-    const lastname = fullname[1];
-    return `${fullname.firstname}, ${fullname.lastname}`;})
-}
+// function nameToAttributes(drivers) {
+//   return drivers.map(function(driver) {
+//     const fullname = driver.split(" ");
+//     const firstname = fullname[0];
+//     const lastname = fullname[1];
+//     return `${fullname.firstname}, ${fullname.lastname}`;})
+// }
 
 function attributesToPhrase(drivers) {
   return drivers.map(function(driver) {
