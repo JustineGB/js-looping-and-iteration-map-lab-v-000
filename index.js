3a134c2061933800f822b9a27c6bcbc99b1e7006
Justine B <justinegayton@gmail.com>
2019-03-04 18:48:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nameToAttributes(drivers) {
 }
 
 function attributesToPhrase(drivers) {
-  
+  return drivers.map(function(driver) {
   // return "${driver.name} is from ${driver.hometown";
 }
 
